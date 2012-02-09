e4ea853b29c15b9649098a289bb6f89797b36237
Hoang Xuan Phu <phunehehe@gmail.com>
2012-02-09 13:54:37 +0700

    restore recipe should be in the DBs

---
+++ postgresql/recipes/restore.rb
@@ -0,0 +1,17 @@
+bash "restore_psql" do
+
+    user "postgres"
+
+    code <<-EOH
+dropuser $USER && echo "User $USER dropped"
+dropdb $DATABASE && echo "Database $DATABASE dropped"
+createuser --no-superuser --no-createdb --no-createrole $USER
+createdb --locale POSIX --encoding UTF8 --template template0 --owner $USER $DATABASE
+psql -U$USER $DATABASE < /vagrant/dump.sql
+    EOH
+
+    environment ({
+        "USER" => node["psql"]["user"],
+        "DATABASE" => node["psql"]["database"],
+    })
+end
