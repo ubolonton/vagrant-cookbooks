cf9115e04ea55deb04b17a869855d262c7c51c43
Hoang Xuan Phu <phunehehe@gmail.com>
2012-02-13 14:01:18 +0700

    better error handle for postgres restore

--- utils/recipes/restore_psql.rb
+++ utils/recipes/restore_psql.rb
@@ -3,8 +3,8 @@ bash "restore_psql" do
     user "postgres"
 
     code <<-EOH
-dropuser $USER && echo "User $USER dropped"
-dropdb $DATABASE && echo "Database $DATABASE dropped"
+dropuser $USER 2> /dev/null
+dropdb $DATABASE 2> /dev/null
 createuser --no-superuser --no-createdb --no-createrole $USER
 createdb --locale POSIX --encoding UTF8 --template template0 --owner $USER $DATABASE
 psql -U$USER $DATABASE < /vagrant/dump.sql
